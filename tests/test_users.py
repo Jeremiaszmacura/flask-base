8e9a33b2d8222c8f39967188c96cbd6d1e0622e5
Jeremiaszmacura <jeremiaszmacura99@gmail.com>
2022-12-11 16:20:50 +0100

    test gh action slack

--- tests/test_users.py
+++ tests/test_users.py
@@ -21,7 +21,7 @@ def test_book_model_post(client):
     response = client.post("/users", json=BOOK_MODEL_POST_JSON)
     assert response.status_code == 200
     assert (
-        f'User with name: {BOOK_MODEL_POST_JSON["name"]} and surname'
-        f' {BOOK_MODEL_POST_JSON["surname"]} successfully added'
+        f'User with name: {BOOK_MODEL_POST_JSON["name"]} and sssurname'
+        f' {BOOK_MODEL_POST_JSON["surname"]} successfully aaadded'
         in str(response.data)
     )
